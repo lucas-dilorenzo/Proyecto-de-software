a869d0bf23b6bc6cb285adc3acf8453f5f412a5a
RENZO AGUSTN CASTIGLIONE <castiglione.renzo@gmail.com>
2025-09-01 21:14:22 -0300

    correccion funcion suma

--- src/calculadora.py
+++
@@ -1,7 +0,0 @@
-def suma(a, b):
-    return a + b
-def calcular(operacion, a, b):
-    if operacion == 'suma':
-        return suma(a, b)
-    else:
-        raise ValueError("Operación no soportada")
