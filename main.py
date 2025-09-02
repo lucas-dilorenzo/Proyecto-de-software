3ef14a434bd900cf05b9506272b08abb9ddff17e
Luna Cuervo <sofialunacuervo99@gmail.com>
2025-09-02 20:28:15 -0300

    descomento calculadora

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     numero = float(input("Ingrese un numero: "))
     otro_numero = float(input("Ingrese otro numero: "))
     operacion = input("Inregrese una operacion (suma, resta, multiplicacion, division): ")
-    # resultado = calculadora.calcular(operacion, numero, otro_numero)
-    # print(resultado) 
+    resultado = calculadora.calcular(operacion, numero, otro_numero)
+    print(resultado) 
