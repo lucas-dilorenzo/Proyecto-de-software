2530b893c77e7c61275a7ec4c469387aaa147ce3
Lucas Di Lorenzo <lucasdilorenzo_89@outlook.com>
2025-09-10 23:13:23 -0300

    - Se agrega comentario para testear los archivos estaticos en distinos SO

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
 from flask import render_template
 
+"""
+Crea la aplicación Flask.
+
+Parámetros:
+- env: str → entorno de ejecución (development, production, etc.)
+- static_folder: str → ruta a la carpeta de archivos estáticos.
+
+    ⚠️ IMPORTANTE:
+    Cambiar la ruta relativa dependiendo el SO que uses para probar la app localmente.
+    # macOS/Linux → usar "../../static"
+    # Windows     → usar r"..\..\static" (doble barra invertida o raw string)
+"""
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
 
