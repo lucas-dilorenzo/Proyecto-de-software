33b2cfe3b21ce7b7f580f15486d7c834ad477ecd
TOMáS MERCANTE <tomiimercante@gmail.com>
2025-09-11 21:46:07 -0300

    Agrego manejadores de error

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, abort
+from .handlers import error
 
 def create_app():
     app = Flask(__name__)
+
+    app.register_error_handler(404, error.not_found)
+    app.register_error_handler(401, error.unauthorized)
+    app.register_error_handler(500, error.generic)
 
     @app.route('/')
     def home():
         return "¡Hola mundo!"
     
+    @app.route('/private')
+    def private():
+        abort(401)
+
+    @app.route('/error')
+    def trigger_error():
+        abort(500)
+
     return app
