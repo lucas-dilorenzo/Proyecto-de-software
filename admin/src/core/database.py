a1ddceadf60d4b1d33303d38998c4d35d2219a48
RENZO AGUSTN CASTIGLIONE <castiglione.renzo@gmail.com>
2025-09-22 19:22:01 -0300

    configuracion base de datos

---
+++ admin/src/core/database.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy_lite import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+db = SQLAlchemy()
+
+def init_app(app):
+    db.init_app(app)
+    return db
+
+def reset_db():
+    print("Resetting database...")
+    Base.metadata.drop_all(bind=db.engine)
+    Base.metadata.create_all(bind=db.engine)
+    print("Database reset complete.")
+
+class Base(DeclarativeBase):
+    pass
---
+++ admin/src/web/config.py
@@ -0,0 +1,41 @@
+from os import environ
+
+class Config(object):
+    """Base configuration."""
+    DEBUG = True
+    TESTING = False
+    SECRET_KEY = "secret"
+
+class ProductionConfig(Config):
+    """Production configuration."""
+
+    DB_HOST = environ.get("DB_HOST")
+    DB_USER = environ.get("DB_USER")
+    DB_PASS = environ.get("DB_PASS")
+    DB_NAME = environ.get("DB_NAME")
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+
+
+class DevelopmentConfig(Config):
+    """Development configuration."""
+
+    DEBUG = True
+    DB_HOST = environ.get("DB_HOST", "localhost")
+    DB_USER = environ.get("DB_USER", "postgres")
+    DB_PASS = environ.get("DB_PASS", "12345")
+    DB_NAME = environ.get("DB_NAME", "grupo08")
+    DB_PORT = environ.get("DB_port", "5432")
+    DB_SCHEME = environ.get("DB_SCHEME", "postgresql")
+    SQLALCHEMY_ENGINES = {
+        'default': f"{DB_SCHEME}://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
+    }
+    # TEMPLATES_AUTO_RELOAD = True
+
+class TestingConfig(Config):
+    """Testing configuration."""
+    TESTING = True
+
+
+config = { "development": DevelopmentConfig, 
+          "test": TestingConfig,
+          "production": ProductionConfig }
