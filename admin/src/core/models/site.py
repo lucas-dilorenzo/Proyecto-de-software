f1cae397731d1257b8feaa5373b3b2060a5db93e
RENZO AGUSTN CASTIGLIONE <castiglione.renzo@gmail.com>
2025-09-22 23:00:20 -0300

    modelo de Sitios Historicos

---
+++ admin/src/core/models/site.py
@@ -0,0 +1,33 @@
+from src.core.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from src.core.database import db
+from datetime import date
+
+
+class Site(Base):
+    __tablename__ = "sites"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    description_short: Mapped[str] = mapped_column(nullable=True)
+    description: Mapped[str] = mapped_column(nullable=True)
+    city: Mapped[str] = mapped_column(nullable=True)
+    province: Mapped[str] = mapped_column(nullable=True)
+    location: Mapped[str] = mapped_column(nullable=True)  # Could be coordinates
+    conservation_status: Mapped[str] = mapped_column(nullable=True)
+    year_declared: Mapped[int] = mapped_column(nullable=True)
+    category: Mapped[str] = mapped_column(nullable=True)
+    registration_date: Mapped[date] = mapped_column(nullable=True)
+    visibility: Mapped[bool] = mapped_column(default=True)
+
+    def __repr__(self) -> str:
+        return f"<Site(id={self.id}, name={self.name}, city={self.city}, province={self.province})>"
+
+    def list_all():
+        return db.session.query(Site).all()
+
+    def get_by_id(site_id: int):
+        return db.session.query(Site).filter(Site.id == site_id).first()
+
+    def get_by_name(name: str):
+        return db.session.query(Site).filter(Site.name == name).first()
--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -3,6 +3,7 @@ from flask import abort
 from flask import render_template
 from src.core import database
 from .handlers import error
+from .config import config
 
 """
 Crea la aplicación Flask.
@@ -14,28 +15,34 @@ Parámetros:
     ⚠️ IMPORTANTE:
     Cambiar la ruta relativa dependiendo el SO que uses para probar la app localmente.
     # macOS/Linux → usar "../../static"
-    # Windows     → usar r"..\..\static" (doble barra invertida o raw string)
+    # Windows     → usar r"..\\..\\static" (doble barra invertida o raw string)
 """
 
 
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
 
+    # Load configuration
+    app.config.from_object(config[env])
+
     app.register_error_handler(404, error.not_found)
     app.register_error_handler(401, error.unauthorized)
     app.register_error_handler(500, error.generic)
 
-    @app.route('/')
+    @app.route("/")
     def home():
-        return render_template('home.html')
-    
-    @app.route('/private')
+        return render_template("home.html")
+
+    @app.route("/private")
     def private():
         abort(401)
 
-    @app.route('/error')
+    @app.route("/error")
     def trigger_error():
         abort(500)
+
+    # Database setup
+    database.init_app(app)
 
     # Register commands
     @app.cli.command("reset-db")
