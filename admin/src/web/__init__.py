ffacfb60c74874c335ce37652a41f87aec73c697
RENZO AGUSTN CASTIGLIONE <castiglione.renzo@gmail.com>
2025-09-24 15:47:03 -0300

    listado de sitios

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,3 +1,4 @@
+from src.web.controllers import sites
 from flask import Flask
 from flask import abort
 from flask import render_template
@@ -29,6 +30,8 @@ def create_app(env="development", static_folder="../../static"):
     app.register_error_handler(404, error.not_found)
     app.register_error_handler(401, error.unauthorized)
     app.register_error_handler(500, error.generic)
+
+    app.register_blueprint(sites.historicalSites_bp)
 
     @app.route("/")
     def home():
