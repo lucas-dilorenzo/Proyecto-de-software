138c662be937cbb9412387fb4bac5376842c04a1
Luna Cuervo <sofialunacuervo99@gmail.com>
2025-09-25 22:49:33 -0300

    Completo el seed y algunas querys necesarias. Arreglo layout

--- admin/src/core/historicalSites/tags/__init__.py
+++ admin/src/core/historicalSites/tags/__init__.py
@@ -7,3 +7,20 @@ from src.core.database import db
 def get_all_tags():
     query = db.session.query(Tag).all()
     return query
+
+# Busca un tag por su nombre
+# Args: name (str): El nombre del tag a buscar.
+# Returns: Tag: El objeto Tag si se encuentra, de lo contrario None.
+def get_tag_by_name(name: str):
+    query = db.session.query(Tag).filter_by(name=name).first()
+    return query
+
+# Crea un nuevo tag en la base de datos
+# Args: kwargs: Los datos del tag (name, slug, description).
+# Returns Tag: El objeto Tag recién creado.
+def create_tag(**kwargs):
+    tag = Tag(**kwargs)
+    session = db.session
+    session.add(tag)
+    session.commit()
+    return tag
