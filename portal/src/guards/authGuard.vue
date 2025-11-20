f2428dfa7450d3c23a7f544e96ae21dd9b98e3dd
RENZO AGUSTN CASTIGLIONE <castiglione.renzo@gmail.com>
2025-11-20 19:17:17 -0300

    agrego guard de autenticacion para resdtringir rutas

---
+++ portal/src/guards/authGuard.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { useAuthStore } from '@/stores/auth'; // Tu store de autenticación
+import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
+
+export const authGuard = (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  // Instanciamos el store AQUÍ DENTRO
+  const authStore = useAuthStore();
+
+  // Lógica de validación
+  if (to.meta.requiresAuth && !authStore.isLoggedIn) {
+    // Si no está logueado, mandar a Login
+    next({ name: 'login' });
+  } else {
+    // Si está logueado o la ruta es pública, continuar
+    next();
+  }
+};
+</script>
