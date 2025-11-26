1934ba66c0578b08680504e1ee3164542487c628
RENZO AGUSTN CASTIGLIONE <castiglione.renzo@gmail.com>
2025-11-26 00:19:09 -0300

    url api google vue

--- portal/src/views/GoogleRedirect.vue
+++ portal/src/views/GoogleRedirect.vue
@@ -4,7 +4,7 @@
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
-const API_URL = import.meta.env.VITE_API_BASE as string;
+const API_URL = import.meta.env.VITE_API_BASE ?? 'https://admin-grupo37.proyecto2025.linti.unlp.edu.ar/api' as string;
 onMounted(() => {
   window.location.href = `${API_URL}/auth/google/login`;
 })
